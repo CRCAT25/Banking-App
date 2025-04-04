23afdced2b4a8b03e38a92d6ed3e7c0f7195799b
Thanh <thanhdev25@gmail.com>
2025-04-04 20:30:43 +0700

    init project

---
+++ src/test/java/net/thanhdevjava/banking/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.thanhdevjava.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
